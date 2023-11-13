59a72df00591802176109b63cf680d26cd8547fd
Javier Jiménez-Alfaro Hacha <javilin7967@gmail.com>
2023-11-13 01:24:11 +0100

    todo preparado para comenzar a trabajar

---
+++ app/src/main/java/com/hachatml/cartamasalta/Caja/Baraja.kt
@@ -0,0 +1,12 @@
+package com.hachatml.cartamasalta.Caja
+
+class Baraja(
+    listaCartas:ArrayList<Carta>
+)
+{
+    companion object{
+        fun crearBaraja(){}
+        fun barajar(){}
+        fun dameCarta(){}
+    }
+}
